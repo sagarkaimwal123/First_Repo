f54a79317abce183e321b5fdf6cb77eba04a0c1a
sagar kaimwal <sagarfresher14@gmail.com>
2023-01-31 19:50:11 +0530

    first commit

--- First_Prog.java
+++ First_Prog.java
@@ -1,7 +1,18 @@
-class First_Prog
+public class Song
 {
-	public static void main(String args[])
+	private String title;
+	private double duration;
+	public Song(String title,double duration)
 	{
-		System.out.println("Hello World");
+		this.title=title;
+		this.duration=duration;
 	}
+	public String getTitle()
+	{
+		return title;
+	}
+	public void setTitle(String title){this.title=title;}
+	public double getDuration(){return duration;}
+	public void setDuration(double duration){this.duration=duration;}
+	public String toString(){return "Song{"+"title='"+title+'\''+",duration="+duration+'}';}
 }
